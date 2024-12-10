cc8e7a146fc8009366e385da0717ab79d7620099
Mạnh Kiên <manhkien203@gmail.com>
2024-12-10 20:53:00 +0700

    initial

---
+++ BaiTapLon/Migrations/20241210093433_tableBookActions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BaiTapLon.Migrations
+{
+    /// <inheritdoc />
+    public partial class tableBookActions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BookAction",
+                columns: table => new
+                {
+                    BookActionId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserId = table.Column<string>(type: "TEXT", nullable: false),
+                    FullName = table.Column<string>(type: "TEXT", nullable: false),
+                    BookId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ActionStatus = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    BorrowDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ReturnDateExpected = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    ReturnDate = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    Note = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookAction", x => x.BookActionId);
+                    table.ForeignKey(
+                        name: "FK_BookAction_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookAction_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "BookId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookAction_BookId",
+                table: "BookAction",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookAction_UserId",
+                table: "BookAction",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookAction");
+        }
+    }
+}
